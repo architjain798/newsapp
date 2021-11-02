c4dd7936b29ff65a27695c929b4506da6d6aeb27
Archit Jain <architjain798@gmail.com>
2021-11-02 13:56:24 +0530

    Pagination added

--- src/components/News.js
+++ src/components/News.js
@@ -6,40 +6,117 @@ export default class News extends Component {
     super();
     this.state = {
       article: [],
+      page: 1,
+      maxPage: 1,
     };
   }
+  apiCall = () => {
+    fetch(
+      `https://newsapi.org/v2/top-headlines?apiKey=1edaf62ee96e415d8660ac6179516a7a&country=in&page=${this.state.page}`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        this.setState({
+          maxPage: Math.ceil(data.totalResults / data.article.length),
+          article: data.articles,
+        });
+        console.log(this.state.article + "-----" + this.state.maxPage);
+      })
+      .catch((e) => console.log(e));
+  };
   componentDidMount() {
     fetch(
-      "https://newsapi.org/v2/top-headlines?apiKey=7a6cc76f520e4a9d9551dc2a1c3b9e82&country=in"
+      `https://newsapi.org/v2/top-headlines?apiKey=1edaf62ee96e415d8660ac6179516a7a&country=in&page=${this.state.page}`
     )
       .then((data) => data.json())
       .then((data) => {
         this.setState({
           article: data.articles,
         });
-        console.log(this.state.article);
+        return data;
+      })
+      .then((data) => {
+        this.setState({
+          maxPage: Math.ceil(data.totalResults / this.state.article.length),
+        });
+        console.log(this.state.article.length + "-----" + this.state.maxPage);
       })
       .catch((e) => console.log(e));
   }
+
+  nextPage = () => {
+    fetch(
+      `https://newsapi.org/v2/top-headlines?apiKey=1edaf62ee96e415d8660ac6179516a7a&country=in&page=${
+        this.state.page + 1
+      }`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        this.setState({
+          article: data.articles,
+          page: this.state.page + 1,
+        });
+      })
+      .catch((e) => console.log(e));
+  };
+
+  prevPage = () => {
+    if (this.state.page > 1) {
+      fetch(
+        `https://newsapi.org/v2/top-headlines?apiKey=1edaf62ee96e415d8660ac6179516a7a&country=in&page=${
+          this.state.page - 1
+        }`
+      )
+        .then((data) => data.json())
+        .then((data) => {
+          this.setState({
+            article: data.articles,
+            page: this.state.page - 1,
+          });
+        })
+        .catch((e) => console.log(e));
+    }
+  };
+
   render() {
     return (
       <div className="container my-3 text-center">
         <h2>NewTak Top News!!!</h2>
         <div className="row">
-          {this.state.article.map((element) => {
-            return (
-              <>
-                <div className="col-md-4">
-                  <NewsItem
-                    title={element.title}
-                    description={element.description}
-                    imageUrl={element.urlToImage}
-                    url={element.url}
-                  />
-                </div>
-              </>
-            );
-          })}
+          {this.state.article != null &&
+            this.state.article.map((element, index) => {
+              return (
+                <>
+                  <div className="col-md-4">
+                    <NewsItem
+                      title={element.title}
+                      description={element.description}
+                      imageUrl={element.urlToImage}
+                      url={element.url}
+                      key={index}
+                    />
+                  </div>
+                </>
+              );
+            })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.prevPage}
+            disabled={this.state.page <= 1}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.nextPage}
+            disabled={this.state.page >= this.state.maxPage}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,24 @@ export default class NewsItem extends Component {
     return (
       <>
         <div className="card my-2" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              imageUrl === null
+                ? "https://miro.medium.com/max/880/0*H3jZONKqRuAAeHnG.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="https://miro.medium.com/max/880/0*H3jZONKqRuAAeHnG.jpg"
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={url} target="_blank" className="btn btn-sm btn-primary">
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-primary"
+            >
               More
             </a>
           </div>
